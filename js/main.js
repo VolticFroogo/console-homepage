6f1407380dd62c6f07a72296faccf02c0751708b
Harry <harry@froogo.co.uk>
2020-07-03 21:19:10 +0100

    More non-desktop device styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    // If the user is on mobile, set the textbox to readonly.
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        $('#textbox').attr('readonly', true);
+    }
+
     registerOptions();
     intro();
 });
